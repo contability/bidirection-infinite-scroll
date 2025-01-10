a019b847621b1cc05a62c1f94508cd5eb32da78f
furium <shouldBnewB@gmail.com>
2025-01-10 17:16:40 +0900

    (add) 상단 스크롤 훅 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,28 @@
 'use client';
 import InfiniteScroll from '@Components/shared/InfiniteScroll';
 import { useState } from 'react';
+import useTopScreenEventHandler from '../../hooks/useTopScreenEventHandler';
 
 const MainPage = () => {
   const [count, setCount] = useState(10);
+  const { onWheel, onTouchEnd, onTouchStart } = useTopScreenEventHandler({
+    isCallable: true,
+    callback: () => console.log('top screen event trigger'),
+    scrollOffset: 20,
+    touchDistanceCriterion: -50,
+    wheelDistanceCriterion: -10,
+  });
   const callbackFunction = () => {
     setCount(state => state + 10);
   };
 
   return (
-    <article className="mx-auto max-w-[720px] text-white">
+    <article
+      className="mx-auto max-w-[720px] text-white"
+      onWheel={onWheel}
+      onTouchStart={onTouchStart}
+      onTouchEnd={onTouchEnd}
+    >
       <h1 className="">양방향 스크롤</h1>
 
       <ul className="flex flex-col">
---
+++ src/hooks/useTopScreenEventHandler.ts
@@ -0,0 +1,82 @@
+import { TouchEventHandler, WheelEventHandler, useEffect, useRef } from 'react';
+
+interface Props {
+  isCallable?: boolean;
+  callback: () => unknown;
+  scrollOffset: number;
+  touchDistanceCriterion: number;
+  wheelDistanceCriterion: number;
+}
+
+interface UseTopScreenEventHandlerResult {
+  onWheel: WheelEventHandler<HTMLDivElement>;
+  onTouchStart: TouchEventHandler<HTMLDivElement>;
+  onTouchEnd: TouchEventHandler<HTMLDivElement>;
+}
+
+function useTopScreenEventHandler({
+  isCallable,
+  callback,
+  scrollOffset,
+  touchDistanceCriterion,
+  wheelDistanceCriterion,
+}: Props): UseTopScreenEventHandlerResult {
+  const wheelPosition = useRef({ default: 0, move: 0, scroll: 0 });
+  const touchPosition = useRef({ start: 0, end: 0 });
+  const scrollPosition = useRef({ default: 0 });
+  const isCalled = useRef(false);
+
+  const onWheel = (event: React.WheelEvent<HTMLDivElement>) => {
+    wheelPosition.current.move = event.deltaY;
+
+    if (
+      wheelPosition.current.move < wheelDistanceCriterion &&
+      scrollPosition.current.default < scrollOffset &&
+      !isCalled.current
+    ) {
+      isCallable && callback();
+      isCalled.current = true;
+
+      return;
+    }
+
+    if (wheelPosition.current.move > wheelDistanceCriterion && isCalled.current) {
+      isCalled.current = false;
+
+      return;
+    }
+  };
+
+  const onTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    touchPosition.current.start = event.changedTouches[0].clientY;
+  };
+
+  const onTouchEnd = (event: React.TouchEvent<HTMLDivElement>) => {
+    touchPosition.current.end = event.changedTouches[0].clientY;
+
+    const { start: touchStart, end: touchEnd } = touchPosition.current;
+
+    if (touchStart - touchEnd < touchDistanceCriterion && scrollPosition.current.default < scrollOffset) {
+      isCallable && callback();
+
+      return;
+    }
+  };
+
+  useEffect(() => {
+    const handleScrollEvent = () => {
+      scrollPosition.current.default = window.scrollY;
+    };
+
+    window.addEventListener('scroll', handleScrollEvent);
+    return () => window.removeEventListener('scroll', handleScrollEvent);
+  }, []);
+
+  return {
+    onWheel,
+    onTouchStart,
+    onTouchEnd,
+  };
+}
+
+export default useTopScreenEventHandler;
